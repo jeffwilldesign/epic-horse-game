a07772e6b9a34d4b4547ce7ea68c5f2d1d736825
Jeff Will <jeff@jeffwilldesign.com>
2022-02-02 18:04:52 -0500

    Set up phaser and capacitor and scaffold iOS app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,26 @@
 import { Component } from '@angular/core';
+import Phaser from 'phaser';
+
+class GameScene extends Phaser.Scene {
+  horse: Phaser.GameObjects.Image;
+
+  constructor(config) {
+    super(config);
+  }
+
+  preload() {
+    this.load.image('horse', 'assets/images/sprites/horse-01.png');
+  }
+
+  create() {
+    console.log('Phaser: create');
+
+    this.horse = this.add.image(100, 300, 'horse');
+  }
+
+  update() {
+  }
+}
 
 @Component({
   selector: 'app-home',
@@ -6,7 +28,23 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  phaserGame: Phaser.Game;
+  config: Phaser.Types.Core.GameConfig;
 
-  constructor() {}
+  constructor() {
+    this.config = {
+      type: Phaser.AUTO,
+      width: 800,
+      height: 600,
+      physics: {
+        default: 'arcade'
+      },
+      parent: 'phaser-game',
+      scene: GameScene
+    };
+  }
 
+  ngOnInit(): void {
+    this.phaserGame = new Phaser.Game(this.config);
+  }
 }
